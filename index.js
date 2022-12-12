b33815093562839075faa34e5305b217aa89c217
Thais B <thais_brt@hotmail.com>
2022-12-12 15:11:39 -0300

    feat: definindo relacionamentos entre artigos e categorias pelo Sequelize

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const connection = require("./database/database");
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
+const Article = require('./articles/Article');
+const Category = require('./categories/Category');
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));

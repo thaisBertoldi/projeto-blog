54dab6aaa3f16cf2fe7879911214d88682e0d381
Thais B <thais_brt@hotmail.com>
2022-12-12 17:52:59 -0300

    feat: cadastro de categorias, adicao de slugify e estilização do formulário

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
 
 router.get('/categories', (req, res) => {
     res.send('Rota categorias')
 });
 
 router.get('/admin/categories/new', (req, res) => {
-    res.send('Criar nova categoria')
+    res.render('admin/categories/new-categorie');
+});
+
+router.post('/categories/save', (req, res) => {
+    if (req.body.title !== undefined) {
+        Category.create({
+            title: req.body.title,
+            slug: slugify(req.body.title)
+        }).then(() => {
+            res.redirect('/');
+        })
+    } else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;

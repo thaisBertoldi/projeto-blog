ab6f5916db3f39df5df1903d728074f6861d403f
Thais B <thais_brt@hotmail.com>
2023-01-06 13:34:24 -0300

    feat: função deletar tarefa com prevenção de erro

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -30,4 +30,19 @@ router.get('/admin/categories', (req, res) => {
     });
 });
 
+router.post('/categories/delete', (req, res) => {
+    const id = req.body.id;
+    if(id != undefined && !isNaN(id)) {
+        Category.destroy({
+            where: {
+                id: id
+            }
+        }).then(() => {
+            res.redirect('/admin/categories');
+        })
+    } else {
+        res.redirect('/admin/categories');
+    }
+});
+
 module.exports = router;

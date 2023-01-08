077aa159249e50944fa6b934842ab9c790a7b00a
Thais B <thais_brt@hotmail.com>
2023-01-08 13:13:04 -0300

    feat: adicionada pagina de criação de novo artigo com tinymce

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
 
 router.get('/articles', (req, res) => {
     res.send('Rota artigo')
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('Criar novo artigo')
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new-articles', { categories: categories })
+    })
 });
 
 module.exports = router;
